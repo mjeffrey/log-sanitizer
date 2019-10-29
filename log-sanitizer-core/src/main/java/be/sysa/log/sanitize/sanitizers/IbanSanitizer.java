c90e8199c187b2a5682a2e4041838a2aef581d2b
Mark Jeffrey <mjeffrey60@gmail.com>
2019-10-29 18:21:13 +0100

    LOG-001 Remove IBANS specific to a country

--- log-sanitizer-core/src/main/java/be/sysa/log/sanitize/sanitizers/IbanSanitizer.java
+++ log-sanitizer-core/src/main/java/be/sysa/log/sanitize/sanitizers/IbanSanitizer.java
@@ -10,9 +10,6 @@ import static java.util.Arrays.asList;
 
 public class IbanSanitizer extends PanSanitizer {
     private static final List<Pattern> patterns = MessageSanitizer.compilePatterns(asList(
-            "BE\\d{2}\\s*\\d{4}\\s*\\d{4}\\s*\\d{4}",
-            "FR\\d{2}\\s*\\d{4}\\s*\\d{4}\\s*\\d{4}\\s*\\d{4}\\s*\\d{4}\\s*\\d{3}",
-            "RO\\d{2}\\s*\\p{Alpha}{4}\\s*\\d{4}\\s*\\d{4}\\s*\\d{4}\\s*\\d{4}",
             "[A-Z]{2,2}[0-9]{2,2}[a-zA-Z0-9]{1,30}"
     ));
 
