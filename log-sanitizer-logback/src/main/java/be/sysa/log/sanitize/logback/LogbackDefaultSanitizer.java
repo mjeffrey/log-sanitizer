0b19dbaea9f939c3011feadadd214bd00274b3a2
Mark Jeffrey <mjeffrey60@gmail.com>
2019-12-22 14:25:14 +0100

    LOG-003 Remove base64 sanitizer from default logback sanitizer as it is too aggressive.

--- log-sanitizer-logback/src/main/java/be/sysa/log/sanitize/logback/LogbackDefaultSanitizer.java
+++ log-sanitizer-logback/src/main/java/be/sysa/log/sanitize/logback/LogbackDefaultSanitizer.java
@@ -2,12 +2,19 @@ package be.sysa.log.sanitize.logback;
 
 import be.sysa.log.sanitize.Buffer;
 import be.sysa.log.sanitize.MessageSanitizer;
+import be.sysa.log.sanitize.sanitizers.*;
 import ch.qos.logback.classic.pattern.MessageConverter;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
 public class LogbackDefaultSanitizer extends MessageConverter {
 
-    private static MessageSanitizer messageSanitizer = MessageSanitizer.all();
+    private static MessageSanitizer messageSanitizer = MessageSanitizer.of(
+                new JsonSanitizer(),
+                new IbanSanitizer(),
+                new PanSanitizer(),
+                new UuidSanitizer(),
+                new ToStringSanitizer()
+    );
 
     public String convert(ILoggingEvent event) {
         return maskSensitiveData(super.convert(event));
